bb880a5318d668e5101bb38336c6eca325c11a99
Jason Chang <j.zisheng@gmail.com>
2020-12-15 14:42:13 -0500

    adding all current files

---
+++ jobApplyService/app.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+if False:
+    # driver = webdriver.Remote("http://127.0.0.1:4444/wd/hub",
+    #                          DesiredCapabilities.CHROME)
+    driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+
+    '''
+    sign in
+    '''
+
+    targetUrl = "https://www.linkedin.com/"+\
+        "login?fromSignIn=true&trk="+\
+        "guest_homepage-basic_nav-header-signin"
+
+    authPath = "/Users/zisheng/auth.txt"
+
+    driver.get(targetUrl)
+
+    f = open(authPath,"r")
+    userStr = f.readline()
+    pwStr = f.readline()
+
+    username = driver.find_element_by_id('username')
+    pw = driver.find_element_by_id('password')
+
+    username.send_keys(userStr)
+    pw.send_keys(pwStr)
+
+
+'''
+go to jobs listings
+'''
+
+def getElemBy(cstr_,class_str):
+    return driver.find_element_by_xpath("//*[contains(@"+cstr_+", '"+class_str+"')]")
+
+def getTextBy(cstr_, class_str):
+    return driver.find_element_by_xpath("//*[contains(@"+cstr_+", '"+class_str+"')]").text
+
+
+jobListsUrl = "https://www.linkedin.com/jobs/search/?geoId=103644278&keywords=%22software%20engineer%22%20%22new%20grad%22&location=United%20States"
+
+driver.get(jobListsUrl)
+
+elems = driver.find_elements_by_xpath("//*[contains(@class, 'job-card-container')]")
+
+job_title_class = "jobs-details-top-card__job-title"
+company_title_class = "jobs-details-top-card__company-url"
+details_id = "job-details"
+for elem in elems:
+    elem.click()
+    title_str = getTextBy("class",job_title_class)
+    company_str = getTextBy("class",company_title_class)
+    details_str = getTextBy("id",details_id)
+    
+    print(company_str,"-",title_str)
+    break
+
+
+# driver.quit()
--- webapp/server.js
+++ webapp/server.js
@@ -1,10 +1,91 @@
-const express = require('express')
-const app = express()
+// https://github.com/WeiChienHsu/webdevbootcamp/tree/master/AuthDemo/views
 
-app.set('view engine', 'ejs')
+const express                 = require("express")    
+const mongoose                = require("mongoose")
+const passport                = require("passport")  
+const bodyParser              = require("body-parser")   
+const User                    = require("./models/user")    
+const LocalStrategy           = require("passport-local")
+const passportLocalMongoose   = require("passport-local-mongoose")
+const app = express();
+
+app.set('view engine','ejs');
+mongoose.connect("mongodb://localhost:27017/accounts",
+		 { useNewUrlParser: true ,  useUnifiedTopology: true});
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(require("express-session")({
+    secret:"secret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.set('view engine','ejs');
+//
+app.use(passport.initialize());
+app.use(passport.session());
+// 
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 app.get('/', (req,res) => {
-    res.send("hello")
+    res.render("home")
 })
+
+app.get('/home', isLoggedIn, (req,res) => {
+    res.render("index",{name: req.user.username})
+})
+
+
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+
+app.post("/register", function(req, res){
+User.register(new User({username:req.body.username}),req.body.password, function(err, user){
+       if(err){
+            console.log(err);
+            return res.render('register');
+        } //user stragety
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/home"); //once the user sign up
+       }); 
+    });
+});
+
+
+
+app.get("/login", function(req, res){
+    res.render("login");
+})
+
+// middleware
+app.post("/login", passport.authenticate("local",{
+    successRedirect:"/home",
+    failureRedirect:"/login"
+}),function(req, res){
+    res.send("User is "+ req.user.id);
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
 
 app.listen(5001)
