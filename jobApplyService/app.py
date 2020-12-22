f420738b8939620add2f4f0a22aa057e03b0f44f
Jason Chang <j.zisheng@gmail.com>
2020-12-22 14:43:29 -0500

    added new pipelines

--- jobApplyService/app.py
+++ jobApplyService/app.py
@@ -49,17 +49,17 @@ if DEBUG:
 
 
 import boto3
-def sendSnsMessage(msg):
+def sendSnsMessage(subject_str,msg_json_str):
     sns = boto3.client('sns',
                        region_name='us-east-1',
                        aws_access_key_id=d["aws_access_key_id"],
                        aws_secret_access_key= d["aws_secret_access_key"])
     # Publish a simple message to the specified SNS topic
     response = sns.publish(
-        TopicArn='arn:aws:sns:us-east-1:337851407731:jobs_topic',   
-        Message='Hello World',   
+        TopicArn = 'arn:aws:sns:us-east-1:337851407731:jobs_topic',
+        Subject = subject_str,
+        Message = msg_json_str
     )
-
     # Print out the response
     print(response)
 
@@ -77,15 +77,24 @@ def searchJobs():
     job_title_class = "jobs-details-top-card__job-title"
     company_title_class = "jobs-details-top-card__company-url"
     details_id = "job-details"
-
     i = 0
-    
     elem = elems[i]
     elem.click()
+    
     title_str = getTextBy("class",job_title_class)
     company_str = getTextBy("class",company_title_class)
     details_str = getTextBy("id",details_id)
-    print(company_str,"  -   ",title_str)
+    url_str = driver.current_url
+    
+    msg = {
+        'title':title_str,
+        'company':company_str,
+        'details_str':details_str,
+        'url':url_str
+    }
+    sendSnsMessage('position',json.dumps(msg))
+
+    # this is the part thats wack
     time.sleep(2)
     action = ActionChains(driver)
     e = getElemBy("class", "jobs-apply-button artdeco-button")
@@ -97,6 +106,6 @@ def applyLever():
     
 
 
-# searchJobs()
-sendSnsMessage("hello")
+searchJobs()
+# sendSnsMessage('test','{"messsage":"Hello World"}')
 # driver.quit()
